27e19f13bf22c0798a0e8e39b9b190f712956ae1
Jorge Tejeda <jorgetejeda0804@gmail.com>
2020-10-18 19:48:22 -0400

    Se creo el controlador para buscar producto por id

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,7 @@
 const Product = require("../models/productModel");
 
+// @desc    Gets All Produtcs
+// @route   GET /api/products
 async function getProducts(req, res) {
   try {
     const products = await Product.findAll();
@@ -10,6 +12,24 @@ async function getProducts(req, res) {
   }
 }
 
-module.exports = {
-    getProducts
+// @desc    Get single Produtc
+// @route   GET /api/product/:id
+async function getProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product Not Found" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ product }));
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 }
+
+module.exports = {
+  getProducts,
+  getProduct,
+};
--- models/productModel.js
+++ models/productModel.js
@@ -1,11 +1,19 @@
 const products = require("../data/products");
 
-function findAll(){
-    return new Promise((resolve,reject)=>{
-        resolve(products);
-    });
+function findAll() {
+  return new Promise((resolve, reject) => {
+    resolve(products);
+  });
+}
+
+function findById(id) {
+  return new Promise((resolve, reject) => {
+    const product = products.find((x) => x.id == id);
+    resolve(product);
+  });
 }
 
 module.exports = {
-    findAll
-}
+  findAll,
+  findById,
+};
