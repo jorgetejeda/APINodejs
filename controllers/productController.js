c040729ac6cdbb9cd98e2487ad95cac39903a165
Jorge Tejeda <jorgetejeda0804@gmail.com>
2020-10-18 20:22:55 -0400

    Se creo la funcionlidad generica para agregar y actualizar un producto

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require("../models/productModel");
+const {getPostData} = require("../utils");
 
 // @desc    Gets All Products
 // @route   GET /api/products
@@ -33,17 +34,9 @@ async function getProduct(req, res, id) {
 // @route   POST /api/products
 async function createProduct(req, res) {
   try {
-
-    let body = "";
-    req.on("data", (chunk) => {
-      //Taking the buffering and convert into string
-      body += chunk.toString();
-    });
-
-    req.on("end", async () => {
-      const { name, description, price } = JSON.parse(body);
-
-      const product = {
+    const body = await getPostData(req);
+    const {name, description, price} = JSON.parse(body);
+    const product = {
         name,
         description,
         price,
@@ -53,7 +46,6 @@ async function createProduct(req, res) {
       res.writeHead(201, { "Content-Type": "application/json" });
 
       return res.end(JSON.stringify(newProduct));
-    });
   } catch (error) {
     console.log(error.message);
   }
--- utils.js
+++ utils.js
@@ -1,14 +1,32 @@
 //Overwrite json file
-const fs = require('fs');
+const fs = require("fs");
+const { resolve } = require("path");
 
-function writeDataToFile(filename, content){
-    fs.writeFileSync(filename,JSON.stringify(content),'utf8',(err)=>{
-        if(err){
-            console.log(err);
-        }
-    })
+function writeDataToFile(filename, content) {
+  fs.writeFileSync(filename, JSON.stringify(content), "utf8", (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 }
+//To Read payload
+function getPostData(req) {
+  return new Promise((resolve, reject) => {
+    try {
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
 
+      req.on("end", () => {
+        resolve(body);
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+}
 module.exports = {
-    writeDataToFile
-}
+  writeDataToFile,
+  getPostData
+};
