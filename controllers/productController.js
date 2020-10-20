d1a5905bfa27e448360d3a3ad5e352326d26a400
Jorge Tejeda Bello <31803682+jorgetejeda@users.noreply.github.com>
2020-10-20 07:45:34 -0400

    Se agregaron los diferentes eventos para ser disparados por el CRUD

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,104 +1,103 @@
 const EventEmitter = require("events");
-const emitter = new EventEmitter();
 const Product = require("../models/productModel");
 const { getPostData } = require("../utils");
 
-// @desc    Gets All Products
-// @route   GET /api/products
-async function getProducts(req, res) {
-  try {
-    const products = await Product.findAll();
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(products));
-    emitter.emit("GET_PRODUCT"); //trigger middleware
-  } catch (error) {
-    console.log(error.message);
+class ProductController extends EventEmitter {
+  // @desc    Gets All Products
+  // @route   GET /api/products
+  async getProducts(req, res) {
+    try {
+      const products = await Product.findAll();
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(products));
+      this.emit("GET_PRODUCT");
+    } catch (error) {
+      console.log(error.message);
+    }
   }
-}
 
-// @desc    Get single Product
-// @route   GET /api/product/:id
-async function getProduct(req, res, id) {
-  try {
+  // @desc    Get single Product
+  // @route   GET /api/product/:id
+  async getProduct(req, res, id) {
+    try {
+      const product = await Product.findById(id);
+      if (!product) {
+        res.writeHead(404, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ message: "Product Not Found" }));
+      } else {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ product }));
+      }
+      this.emit("GET_PRODUCT_BY_ID", { id: product.id || 0 });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  // @desc    Create a Product
+  // @route   POST /api/products
+  async createProduct(req, res) {
+    try {
+      const body = await getPostData(req);
+      const { name, description, price } = JSON.parse(body);
+      const product = {
+        name,
+        description,
+        price
+      };
+
+      const newProduct = await Product.create(product);
+      res.writeHead(201, { "Content-Type": "application/json" });
+
+      res.end(JSON.stringify(newProduct));
+      this.emit("POST_PRODUCT", { newProduct });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  // @desc    Update a Product
+  // @route   PUT /api/products/:id
+  async updateProduct(req, res, id) {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Products Not Found" }));
+    } else {
+      const body = await getPostData(req);
+
+      const { name, description, price } = JSON.parse(body);
+
+      //If there is any of item that wasn't updated
+      const productData = {
+        name: name || product.name,
+        description: description || product.description,
+        price: price || product.price
+      };
+
+      const updateProduct = await Product.update(productData, id);
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(updateProduct));
+      this.on("PUT_PRODUCT", { updateProduct });
+    }
+  }
+
+  // @desc    Delete a Product
+  // @route   DELETE /api/products/:id
+  async deleteProduct(req, res, id) {
     const product = await Product.findById(id);
     if (!product) {
       res.writeHead(404, { "Content-Type": "application/json" });
       res.end(JSON.stringify({ message: "Product Not Found" }));
     } else {
+      const deleteProduct = await Product.deleteById(id);
       res.writeHead(200, { "Content-Type": "application/json" });
-      res.end(JSON.stringify({ product }));
+      res.end(JSON.stringify(deleteProduct));
     }
-  } catch (error) {
-    console.log(error.message);
+    this.on("DELETE_PRODUCT", { id });
   }
 }
 
-// @desc    Create a Product
-// @route   POST /api/products
-async function createProduct(req, res) {
-  try {
-    const body = await getPostData(req);
-    const { name, description, price } = JSON.parse(body);
-    const product = {
-      name,
-      description,
-      price
-    };
-
-    const newProduct = await Product.create(product);
-    res.writeHead(201, { "Content-Type": "application/json" });
-
-    return res.end(JSON.stringify(newProduct));
-  } catch (error) {
-    console.log(error.message);
-  }
-}
-
-// @desc    Update a Product
-// @route   PUT /api/products/:id
-async function updateProduct(req, res, id) {
-  const product = await Product.findById(id);
-
-  if (!product) {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "Products Not Found" }));
-  } else {
-    const body = await getPostData(req);
-
-    const { name, description, price } = JSON.parse(body);
-
-    //If there is any of item that wasn't updated
-    const productData = {
-      name: name || product.name,
-      description: description || product.description,
-      price: price || product.price
-    };
-
-    const updateProduct = await Product.update(productData, id);
-
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(updateProduct));
-  }
-}
-
-// @desc    Delete a Product
-// @route   DELETE /api/products/:id
-async function deleteProduct(req, res, id) {
-  const product = await Product.findById(id);
-  if (!product) {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({ message: "Product Not Found" }));
-  } else {
-    const deleteProduct = await Product.deleteById(id);
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(deleteProduct));
-  }
-}
-
-module.exports = {
-  getProducts,
-  getProduct,
-  createProduct,
-  updateProduct,
-  deleteProduct
-};
+module.exports = ProductController;
--- index.js
+++ index.js
@@ -2,49 +2,61 @@ const http = require("http");
 const EventEmitter = require("events");
 const emitter = new EventEmitter();
 
-const {
-  getProducts,
-  getProduct,
-  createProduct,
-  updateProduct,
-  deleteProduct
-} = require("./controllers/productController");
+const ProductController = require("./controllers/productController");
+const product = new ProductController();
 
-emitter.on("GET_PRODUCT", () => {
-  console.log("Se obtuvo el producto");
+//Listener
+product.on("GET_PRODUCT", () => {
+  console.log("Get product");
+});
+
+product.on("GET_PRODUCT_BY_ID", (arg) => {
+  console.log("Get product by id", arg);
+});
+
+product.on("POST_PRODUCT", (arg) => {
+  console.log("Post product", arg);
+});
+
+product.on("PUT_PRODUCT", (arg) => {
+  console.log("Update product by id", arg);
+});
+
+product.on("DELETE_PRODUCT", (arg) => {
+  console.log("Delete product by id", arg);
 });
 
 const server = http.createServer((req, res) => {
   if (req.url === "/api/products" && req.method === "GET") {
-    getProducts(req, res);
+    product.getProducts(req, res);
   } else if (
     req.url.match(/\/api\/products\/([0-9]+)/) &&
     req.method === "GET"
   ) {
     const id = req.url.split("/")[3]; // api/products/1
-    getProduct(req, res, id);
+    product.getProduct(req, res, id);
   } else if (req.url === "/api/products" && req.method === "POST") {
-    createProduct(req, res);
+    product.createProduct(req, res);
   } else if (
     req.url.match(/\/api\/products\/([0-9]+)/) &&
     req.method === "PUT"
   ) {
     const id = req.url.split("/")[3]; // api/products/1
-    updateProduct(req, res, id);
+    product.updateProduct(req, res, id);
   } else if (
     req.url.match(/\/api\/products\/([0-9]+)/) &&
     req.method === "DELETE"
   ) {
     const id = req.url.split("/")[3];
-    deleteProduct(req, res, id);
+    product.deleteProduct(req, res, id);
   } else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "Route Not Found" }));
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-  console.log("Running 5000");
+  console.log("Running 4000");
 });
